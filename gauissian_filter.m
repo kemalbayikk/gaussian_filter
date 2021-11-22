4d883c70804f32bf9e36f3ce77839138c0118ea0
Kemal Bayık <45203337+ketowk@users.noreply.github.com>
2021-11-22 22:31:59 +0300

    Update and rename hw2soru1_151101060.m to gauissian_filter.m

--- gauissian_filter.m
+++ gauissian_filter.m
@@ -2,7 +2,6 @@ Img = imread('cameraman.tif');
 
 figure,imshow(Img);
 gaussianFilter(Img);
-boxFilter(Img);
 
 
 function gaussianFilter(Img)
@@ -24,25 +23,4 @@ end
 Output = uint8(Output);
 figure,imshow(Output);
 
-end
-
-
-function boxFilter(Img)
-[x,y] = size(Img);
-weight = 1;
-I = padarray(Img,[10 10]);
-for i = 1:x-1
-    for j = 1:y-1
-        sum = uint16(0);
-        for ii = i:i+21  
-            for jj = j:j+21
-                sum = sum + uint16(I(ii, jj))*weight;
-            end
-        end
-        I2(i,j) = floor(sum/(441*weight));
-    end
-end
-I2 = uint8(I2);
-figure
-imshow(I2)
 end
